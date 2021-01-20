85d1ac1f2cac2206f62547601cd49b1084c86263
Tiago Emerenciano <emerenciano.tiago@gmail.com>
2021-01-19 21:54:33 -0300

    Inserção dos dados no PostgreSQL

--- fundamentus/fundamentus/pipelines.py
+++ fundamentus/fundamentus/pipelines.py
@@ -6,20 +6,116 @@
 
 # useful for handling different item types with a single interface
 import json
+import psycopg2
 
 from itemadapter import ItemAdapter
 from scrapy.exporters import JsonItemExporter
+import logging
 
-class FundamentusPipeline(object):
-    def open_spider(self, spider):
-        self.file = open("Fundamentos.json", 'wb')
-        self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
-        self.exporter.start_exporting()
+logger = logging.getLogger('pipeline')
 
-    def close_spider(self, spider):
-        self.exporter.finish_exporting()
-        self.file.close()
+class FundamentusPostgrePipeline(object):
+  def open_spider(self, spider):
+    hostname = 'localhost'
+    username = 'postgres'
+    password = 'postgres'
+    database = 'postgres'
+    port = 15432
+    self.connection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database, port=port)
+    self.cursor = self.connection.cursor()
 
-    def process_item(self, item, spider):
-        self.exporter.export_item(item)
-        return item
+    # self.file = open("Fundamentos.json", 'wb')
+    # self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
+    # self.exporter.start_exporting()
+
+  def close_spider(self, spider):
+    self.cursor.close()
+    self.connection.close()
+
+    # self.exporter.finish_exporting()
+    # self.file.close()
+
+  def process_item(self, item, spider):
+    try:
+      self.cursor.execute("""
+      INSERT INTO fundamentos.papeis(codigo) 
+      VALUES ('{0}') 
+      ON CONFLICT (codigo) DO NOTHING""".format(item['papel']))
+      
+      self.cursor.execute("""
+      INSERT INTO fundamentos.fundamentos 
+      (
+        id, 
+        preco_por_lucro, 
+        preco_por_valorpatrimonial, 
+        psr, 
+        dividend_yield, 
+        preco_por_ativos, 
+        preco_por_capitaldegiro, 
+        preco_por_ativocirculanteliquido, 
+        preco_por_ebit, 
+        ev_por_ebit, 
+        ev_por_ebitda, 
+        margem_ebit, 
+        margem_liquida, 
+        liquidez_corrente, 
+        roic, 
+        roe, 
+        liquidez_dois_meses, 
+        divida_bruta_por_patrimonio, 
+        crescimento_receita_cinco_anos, 
+        papel, 
+        data_alteracao, 
+        patrimonio_liquido 
+      ) VALUES (gen_random_uuid(), {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, {12}, {13}, {14}, {15}, {16}, {17}, '{18}', CURRENT_TIMESTAMP, {19}) ON CONFLICT (papel) DO 
+        UPDATE SET 
+          preco_por_lucro = {0},
+          preco_por_valorpatrimonial = {1},
+          psr = {2},
+          dividend_yield = {3},
+          preco_por_ativos = {4},
+          preco_por_capitaldegiro = {5},
+          preco_por_ativocirculanteliquido = {6},
+          preco_por_ebit = {7},
+          ev_por_ebit = {8},
+          ev_por_ebitda = {9},
+          margem_ebit = {10},
+          margem_liquida = {11},
+          liquidez_corrente = {12},
+          roic = {13},
+          roe = {14},
+          liquidez_dois_meses = {15},
+          divida_bruta_por_patrimonio = {16},
+          crescimento_receita_cinco_anos = {17},
+          data_alteracao = CURRENT_TIMESTAMP,
+          patrimonio_liquido = {19}
+        """.format(
+        item['pl'].replace('.', '').replace(',', '.'), 
+        item['pvp'].replace('.', '').replace(',', '.'),
+        item['psr'].replace('.', '').replace(',', '.'), 
+        item['dy'].replace('%', '').replace('.', '').replace(',', '.'), 
+        item['pativo'].replace('.', '').replace(',', '.'), 
+        item['pcapgiro'].replace('.', '').replace(',', '.'), 
+        item['pativ_circ_liq'].replace('.', '').replace(',', '.'), 
+        item['pebit'].replace('.', '').replace(',', '.'), 
+        item['evebit'].replace('.', '').replace(',', '.'), 
+        item['evebitda'].replace('.', '').replace(',', '.'), 
+        item['mrg_ebit'].replace('%', '').replace('.', '').replace(',', '.'), 
+        item['mrg_liq'].replace('%', '').replace('.', '').replace(',', '.'), 
+        item['liq_corr'].replace('.', '').replace(',', '.'), 
+        item['roic'].replace('%', '').replace('.', '').replace(',', '.'), 
+        item['roe'].replace('%', '').replace('.', '').replace(',', '.'), 
+        item['liq_dois_meses'].replace('.', '').replace(',', '.'), 
+        item['div_brut_patrim'].replace('.', '').replace(',', '.'), 
+        item['cresc_rec_cinco_anos'].replace('%', '').replace('.', '').replace(',', '.'), 
+        item['papel'].replace('.', '').replace(',', '.'), 
+        item['patrim_liq'].replace('.', '').replace(',', '.')
+      ))
+      self.connection.commit()
+    except psycopg2.DatabaseError as error:
+      logger.error(error)
+      self.connection.rollback()
+      pass
+
+    # self.exporter.export_item(item)
+    return item
